d431c97d3c05ffb4e8d5bbece7c2491e3c4048b5
Xin Zhang <xin.zhang@essentialenergy.com.au>
2017-10-12 22:35:12 +1100

    express upload

---
+++ index.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const app = express();
+
+app.use(fileUpload());
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
+
+app.get('/', (req, res) => {
+    res.send(`
+        <form action="/upload" enctype="multipart/form-data" method="post">
+            <input type="file" name="foo" /><br/><br/>
+            <input type="submit" value="upload" />
+        </form>
+    `)
+})
+
+app.post('/upload', (req, res) => {
+    if (!req.files) return res.status(400).send('No files were uploaded.');
+
+    const {foo} = req.files;
+    const uploadTo = `uploads/${foo.name}`
+
+    foo.mv(uploadTo, (err) => {
+        if (err) return res.status(500).send(err);
+
+        res.send(`File uploaded to <a href=${uploadTo}>${uploadTo}</a>`)
+    });
+
+});
+
+app.listen(8083, ()=> {
+    console.log('server listening on port 8083')
+})
